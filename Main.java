75eb2437ce96c5a4b6edeae08f2607ec2bf71dcf
justUser <eldar.konovalov@mail.ru>
2025-02-04 22:36:45 +0400

    Create Person class

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Person person1 = new Person("Tom");
+        System.out.println(person1.getName());
     }
 }
